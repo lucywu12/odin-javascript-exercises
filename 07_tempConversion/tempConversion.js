2d86684d0c4af2d40827796500f6117b8a85623f
Lucy Wu <lucy.wu012@gmail.com>
2024-01-11 18:11:17 -0500

    after some numerical difficulty... we finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let fahrenheit = (temp - 32) * (5/9);
+  fahrenheit = Math.round(fahrenheit * 10) / 10;
+  return fahrenheit;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let celcius = (temp * (9/5)) + 32;
+  celcius = Math.round(celcius * 10) / 10;
+  return celcius;
 };
 
 // Do not edit below this line
