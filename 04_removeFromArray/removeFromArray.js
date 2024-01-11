72f7d3e5cf11d006e4fc7072367825c39ec57054
Lucy Wu <lucy.wu012@gmail.com>
2024-01-11 17:21:41 -0500

    finished, altnerative ways with forEach loop

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const arr = [];
 
+    for (let i = 0; i < array.length; i++) {
+        if (!args.includes(array[i])) {
+            arr.push(array[i]);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
