318982edbe2d22cfc5e68cba1121743cb4153800
Lucy Wu <lucy.wu012@gmail.com>
2024-01-11 17:56:27 -0500

    completed leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,18 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let divByFour = year % 4;
+    let divByHundred = year % 100;
+    let divByFourHundred = year % 400;
 
+    if (divByFour == 0) {
+        if (divByHundred == 0) {
+            if (divByFourHundred == 0) {
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+    return false;
 };
 
 // Do not edit below this line
