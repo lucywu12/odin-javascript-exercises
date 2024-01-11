78f2440b12aa4695203ec8bd077921cb47d43004
Lucy Wu <lucy.wu012@gmail.com>
2024-01-11 16:57:20 -0500

    finished second exercise, all tests pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let s = "";
+    if (num < 0) {
+        return "ERROR";
+    }
+    for (let i = 0; i < num; i++) {
+        s += string;
+    }
+    return s;
 };
 
 // Do not edit below this line
