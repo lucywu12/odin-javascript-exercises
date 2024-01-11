2f04a359bd49730cc3ba12a7786a79dce905a2ff
Lucy Wu <lucy.wu012@gmail.com>
2024-01-11 17:50:33 -0500

    passed all tests for ex 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if ((typeof num1 != "number") || (typeof num2 != "number")) {
+        return "ERROR";
+    }
 
-};
+    if ((num1 < 0) || (num2 < 0)) {
+        return "ERROR";
+    }
+
+    // Swap in case the first is bigger than second
+    if (num1 > num2) {
+        tmp = num1;
+        num1 = num2;
+        num2 = tmp;
+    }
+
+    let sum = 0;
+    for (let i = num1; i < num2 + 1; i++) {
+        sum += i;
+    }
+    return sum;
+ };
 
 // Do not edit below this line
 module.exports = sumAll;
