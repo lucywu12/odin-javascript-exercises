a9530a07507c7a5509408cad13fc7a3cb3a571f2
Lucy Wu <lucy.wu012@gmail.com>
2024-01-13 17:00:47 -0500

    my solution is more complex, but general idea is there

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    //preprocess
+    let lower = string.toLowerCase();
+    let replaced = lower.replace(/[!, .]/g, ""); //letters and numbers only
 
+    var letters = replaced.split(''); //make array
+    //less than midpoint O(n/2) solution hopefully
+    const len = letters.length;
+    const mid = len / 2;
+    console.log(letters);
+    for (let i = 0; i < mid; i++) {
+        if (letters[i] != letters[len - 1 - i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
