3409109133508fa56b433e0669db9c9778be0c2e
Lucy Wu <lucy.wu012@gmail.com>
2024-01-13 17:10:25 -0500

    finished fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(target) {
+    //make series and store in array
+    let nums = [1, 1];
 
+    if (target < 0) {
+        return "OOPS"; //no negatives!
+    } else if (target == 0) {
+        return 0;
+    } else if (target < 3) {
+        return 1;
+    } else {
+        for (let i = 2; i < target; i++) {
+            nums.push(nums[i - 1] + nums[i - 2]);
+        }
+        return nums[target - 1];
+    }
 };
 
 // Do not edit below this line
