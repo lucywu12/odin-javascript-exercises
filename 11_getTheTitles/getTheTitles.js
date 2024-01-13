90532199f53b56b7920c93093365576848cc2c93
Lucy Wu <lucy.wu012@gmail.com>
2024-01-13 17:17:59 -0500

    used forEach but can also use map

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,10 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let list = [];
+    arr.forEach(book => list.push(book.title));
+    return list;
 
+    //alternatively
+    //return arr.map(book => book.title);
 };
 
 // Do not edit below this line
