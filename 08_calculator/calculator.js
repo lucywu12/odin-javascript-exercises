e1ecd88937302ca93582bff03a54699f0d5b791e
Lucy Wu <lucy.wu012@gmail.com>
2024-01-13 16:24:40 -0500

    finished calculator, minimal error handling however

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s = 0;
+  arr.forEach(item => s += item); //iterate to add?
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let m = 1;
+  arr.forEach(item => m *= item);
+  return m;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	let f = 1;
+  for (let i = 1; i <= num; i++) {
+    f *= i;
+  }
+  return f;
 };
 
 // Do not edit below this line
