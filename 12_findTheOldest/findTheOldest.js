3afe454904b623bb7080d77d5724e0523b9e020d
Lucy Wu <lucy.wu012@gmail.com>
2024-01-13 17:40:04 -0500

    finished sorting ages, can also be done with reduce

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let key = "yearOfDeath";
+    const date = new Date();
+    const todayYear = date.getFullYear();
+    const person = arr.sort((a, b) => {
+        let aAge = 0;
+        let bAge = 0;
+        if (key in a) {
+            aAge = a.yearOfDeath - a.yearOfBirth;
+        } else {
+            aAge = todayYear - a.yearOfBirth;
+        }
+        if (key in b) {
+            bAge = b.yearOfDeath - b.yearOfBirth;
+        } else {
+            bAge = todayYear - b.yearOfBirth;
+        }
+        if (aAge > bAge) {
+            return -1;
+        } else if (aAge < bAge) {
+            return 1;
+        } else {
+            return 0;
+        }
+    });
 
+    console.table(person);
+    return arr[0]; //first should be the oldest
 };
 
 // Do not edit below this line
